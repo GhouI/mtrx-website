16beee14b3da0e3e841620a163f9eec311ea20c2
Ghoui <flareonverm@outlook.com>
2025-12-26 21:00:54 +0000

    added psuedo stats

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,5 +1,23 @@
 "use client"
 import Stats from "@/components/stats/stats"
-export default async function Overview() {
-  return <div>Hello</div>;
+export default  function Home() {
+  const currentStats = {
+    "deepsearch" : 0, 
+    "Jobs" : 0,
+  }
+
+  return <div>
+    <Stats stats={
+      [
+        {
+          "name" : "Deep search workflows",
+          "value" : currentStats.deepsearch
+        },
+        {
+          "name": "Jobs",
+          "value" : currentStats.Jobs
+        }
+      ]
+    }/>
+  </div>;
 }
--- components/stats/stats.tsx
+++ components/stats/stats.tsx
@@ -3,22 +3,24 @@ import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 
 
-type statsprops = {
+type Stats = {
   name: string, 
   value: string | number,
 }
-
-export default function StatsComponent(Stats: statsprops[]) {
+type StatsComponentProps = {
+  stats: Stats[]
+}
+export default function StatsComponent({stats} : StatsComponentProps) {
   return (
     <div className="flex items-center justify-center p-10">
       <div className="mx-auto grid grid-cols-1 gap-px rounded-xl bg-border sm:grid-cols-2 lg:grid-cols-4">
-        {Stats.map((stat,index) => (
+        {stats.map((stat,index) => (
           <Card
             key={stat.name}
             className={cn(
               "rounded-none border-0 shadow-none py-0",
               index === 0 && "rounded-l-xl",
-              index === Stats.length - 1 && "rounded-r-xl"
+              index === stats.length - 1 && "rounded-r-xl"
             )}
           >
             <CardContent className="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 p-4 sm:p-6">
