396ed075f58be6c9dae35f1454ea3ea656614be8
Ghoui <flareonverm@outlook.com>
2025-12-26 15:35:38 +0000

    migrating

--- app/(dashboard)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import Sidebar01 from "@/components/sidebar-01"
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
-      <Sidebar01 />
-      {children}
-    </div>
-  )
-}
