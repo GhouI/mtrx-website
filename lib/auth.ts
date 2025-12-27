eef40d9b158c8ca041716fa82cfbcf178a8d1fba
Ghoui <flareonverm@outlook.com>
2025-12-27 14:45:54 +0000

    changd to prisma adapter for auth, added scopes

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,21 @@
 import { betterAuth } from "better-auth";
-import {Pool} from "pg"
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import client from "@/lib/db/client"
+
+
 export const auth = betterAuth({
-  database: new Pool({
-    connectionString: process.env.DATABASE_URL
+  database: prismaAdapter(client, {
+    provider: 'postgresql'
   }),
   socialProviders: {
     google: {
+      accessType: "offline", 
+      prompt: "select_account consent", 
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      scope: ['profile', 'https://www.googleapis.com/auth/drive.file',
+         'https://www.googleapis.com/auth/documents', 'https://www.googleapis.com/auth/drive.readonly',
+        'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.metadata']
     }
   }
 });
